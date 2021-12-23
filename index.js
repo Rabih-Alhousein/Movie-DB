b56e9b512aabc852cb7f821a23d78d3953a37506
Rabih-Alhousein <rabih.alhousein@gmail.com>
2021-12-24 00:37:31 +0200

    step 4

--- index.js
+++ index.js
@@ -19,11 +19,48 @@ app.get("/test", (req, res) => {
 // Time Route
 app.get('/time', (req, res) => {
     var date = new Date()
-    res.send ({
+    res.send({
         status: 200,
         message: date.getHours() + ":" + date.getMinutes()
     });
 })
+
+// Hello  Route
+app.get("/hello/:id", (req, res) => {
+    res.send({
+        status: 200,
+        message: 'Hello ' + req.params.id + '!'
+    })
+})
+
+
+app.get("/hello/", (req, res) => {
+    res.send({
+        status: 200,
+        message: 'Hello'
+    })
+})
+
+
+// Search Route
+app.get("/search/", (req, res) => {
+    if (req.query.s) {
+        res.send({
+            status: 200,
+            message: 'ok',
+            data: req.query.s
+        })
+    } else{
+        res.send({
+            staus: 500,
+            error: true,
+            message: 'you have to provide a search'
+        })
+    }
+})
+
+
+
 
 
 app.listen(3000)
