6822a7801b0f7b5189b57d08c8fc0e785ab26b34
Rabih-Alhousein <rabih.alhousein@gmail.com>
2021-12-23 23:55:00 +0200

    step 3

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express")
-const app= express()
+const app = express()
 
-//Create the home route
-app.get("/", (req,res) => {
+// Home Route
+app.get("/", (req, res) => {
     res.status(200).send("ok")
 })
 
+// Test Route
+
+app.get("/test", (req, res) => {
+    res.send({
+        status: 200,
+        message: "ok"
+    });
+})
+
+
+// Time Route
+app.get('/time', (req, res) => {
+    var date = new Date()
+    res.send ({
+        status: 200,
+        message: date.getHours() + ":" + date.getMinutes()
+    });
+})
+
+
 app.listen(3000)
+
+
+
+
