cde77c1e7f0b87a0fe5f52ebee907c48c9fbef5a
Rabih-Alhousein <rabih.alhousein@gmail.com>
2022-01-13 00:54:28 +0200

    step 12

--- index.js
+++ index.js
@@ -2,47 +2,6 @@ const express = require("express")
 const app = express()
 
 
-// Home Route
-app.get("/", (req, res) => {
-    res.status(200).send("ok")
-})
-
-// Test Route
-
-app.get("/test", (req, res) => {
-    res.send({
-        status: 200,
-        message: "ok"
-    });
-})
-
-
-// Time Route
-app.get('/time', (req, res) => {
-    var date = new Date()
-    res.send({
-        status: 200,
-        message: date.getHours() + ":" + date.getMinutes()
-    });
-})
-
-// Hello  Route
-app.get("/hello/:id", (req, res) => {
-    res.send({
-        status: 200,
-        message: 'Hello ' + req.params.id + '!'
-    })
-})
-
-
-app.get("/hello/", (req, res) => {
-    res.send({
-        status: 200,
-        message: 'Hello'
-    })
-})
-
-
 
 const movies = [
     { title: 'Jaws', year: 1975, rating: 8 },
@@ -52,89 +11,101 @@ const movies = [
 ]
 
 
-// Search Route
-app.get("/search/", (req, res) => {
-    if (req.query.s) {
-        res.send({
-            status: 200,
-            message: 'ok',
-            data: req.query.s
-        })
-    } else {
-        res.send({
-            staus: 500,
-            error: true,
-            message: 'you have to provide a search'
-        })
+// ANCHOR MongoDB
+
+var mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://rabih:1234@cluster0.wkivl.mongodb.net/mongodb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to mongodb'))
+    .catch((err) => console.log(err))
+const Schema = mongoose.Schema
+
+const MovieSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    year: {
+        type: Number,
+        require: true
+    },
+    rating: {
+        type: Number,
+        default: 4
     }
 })
 
 
+var Movies = mongoose.model('movies', MovieSchema);
+//uploading the array of movies objects to mongodb for the first time
+/* movies.map((row) => {
+    console.log(row);
+    var movie = new Movies(row);
+    movie.save()
+        .then((result) => console.log('movies uploaded to'))
+        .catch((err) => console.log(err))
+}); */
 
 
-// Read Route
-app.get("/movies/get", (req, res) => {
-    res.send({
-        status: 200,
-        data: movies
-    })
+
+// ANCHOR Read Route
+//http://localhost:3000/movies/read/
+
+app.get("/movies/read", function (req, res) {
+    Movies.find()
+        .then((result) => res.send(result))
+        .catch((err) => console.log(err))
 })
 
 
 
-// Order by Date
+// ANCHOR Order by Date Route
+//http://localhost:3000/movies/read/by-date
+
+
 app.get("/movies/read/by-date", (req, res) => {
-    let SortedMovies = movies.slice().sort(function (a, b) { return b.year - a.year })
-    res.send({
-        status: 200,
-        data: SortedMovies
-    })
-
+    Movies.find()
+        .then(moviesData => {
+            res.send({ status: 200, data: moviesData.sort((a, b) => b.year - a.year) })
+        }).catch(err => {
+            console.log("error, no entry found")
+        })
 })
 
 
-// Order by Rating
+// ANCHOR Order by Rating Route
+//http://localhost:3000/movies/read/by-rating
+
+
 app.get("/movies/read/by-rating", (req, res) => {
-    let SortedMovies = movies.slice().sort(function (a, b) { return b.rating - a.rating })
-    res.send({
-        status: 200,
-        data: SortedMovies
-    })
+    Movies.find()
+        .then(moviesData => {
+            res.send({
+                status: 200, data: moviesData.sort((a, b) => b.rating - a.rating)
+            })
+        }).catch(err => {
+            console.log("error, no entry found")
+        })
+});
 
-})
+
+// ANCHOR Order by Title Route
+//http://localhost:3000/movies/read/by-title
 
 
-// Order by Title
 app.get("/movies/read/by-title", (req, res) => {
-    let SortedMovies = movies.slice().sort(function (a, b) { return (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0) })
-    res.send({
-        status: 200,
-        data: SortedMovies
-    })
-
-})
-
-
-// Read One Route
-app.get("/movies/read/:id", (req, res) => {
-    if (req.params.id >= 0 && req.params.id < movies.length) {
-        res.send({
-            status: 200,
-            data: movies[req.params.id]
+    Movies.find()
+        .then(moviesData => {
+            res.send({
+                status: 200, data: moviesData.sort((a, b) => (a.title).localeCompare(b.title))
+            })
+        }).catch(err => {
+            console.log("error, no entry found")
         })
-    } else {
-        res.status(404)
-        res.send({
-            status: 404,
-            error: true,
-            message: 'the movie ' + req.params.id + ' does not exist'
-        })
-    }
-})
+});
 
-//  Create Route
-
+// ANCHOR Create Route
 //http://localhost:3000/movies/add?title=movie&year=1999&rating=9
+
 app.post("/movies/add", (req, res) => {
     let title = req.query.title
     let year = req.query.year
@@ -156,58 +127,65 @@ app.post("/movies/add", (req, res) => {
         } else {
             year = parseInt(year)
             rating = parseFloat(rating)
-            movies.push({ title, year, rating })
-            res.send({
-                status: 200,
-                data: movies
-            })
+            let movie = new Movies({ title, year, rating })
+            movie.save()
+                .then(newMovie => {
+                    Movies.find()
+                        .then(moviesData => {
+                            res.send({ status: 200, data: moviesData });
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
     }
 })
 
+// ANCHOR Delete Route
+//http://localhost:3000/movies/delete/61dcd26da7cefacd20d01394
 
-
-// Delete Route
 app.delete("/movies/delete/:id", (req, res) => {
     const id = req.params.id
-    if (id < 0 || (id > movies.length - 1) || isNaN(id)) {
-        res.send({
-            status: 404,
-            error: true,
-            message: 'the movie ' + id + ' does not exist'
-        })
-    } else {
-        movies.splice(id, 1)
-        res.send({
-            status: 200,
-            data: movies,
-        })
-    }
+    Movies.findByIdAndDelete(id).then(deletedMovie => {
+        Movies.find()
+            .then(moviesData => {
+                res.send({ status: 200, data: moviesData });
+            }).catch(err => {
+                console.log(err)
+            })
+    }).catch(err => {
+        res.status(404).send({ status: 404, error: true, message: `the movie '${id}' does not exist ` });
+    })
 })
 
-// Update Route
-// http://localhost:3000/movies/edit/2?title=Deadpool&year=2016&rating=8
+// ANCHOR Update Route
+// http://localhost:3000/movies/edit/61dcd26da7cefacd20d01394?title=Jaws&year=1975&rating=8
+
 app.put("/movies/edit/:id", (req, res) => {
-    const id = req.params.id
-    if (id < 0 || (id > movies.length - 1) || isNaN(id)) {
-        res.send({
-            status: 404,
-            error: true,
-            message: 'the movie ' + id + ' does not exist'
-        })
-    } else {
+    Movies.findById(req.params.id).then(async (doc, err) => {
         let title = req.query.title
         let year = req.query.year
         let rating = req.query.rating
-        if (title != undefined) movies[id].title = title
-        if (year != undefined && !isNaN(year) && year > 1900 && year <= 2022) movies[id].year = year
-        if (rating != undefined && !isNaN(rating) && rating >= 0 && rating <= 10) movies[id].rating = rating
-        res.send({
-            status: 200,
-            data: movies,
-        })
+        if (title != undefined) doc.title = title
+        if (year != undefined && !isNaN(year) && year > 1900 && year <= 2022) doc.year = year
+        if (rating != undefined && !isNaN(rating) && rating >= 0 && rating <= 10) doc.rating = rating
+        await doc.save()
+        Movies.find()
+            .then(moviesData => {
+                res.send({ status: 200, data: moviesData });
+            }).catch(err => {
+                console.log(err)
+            })
+            .catch((err) => console.log(err))
 
-    }
-})
+    }).catch(err => {
+        res.status(404).send({ status: 404, error: true, message: `the movie '${req.params.id}' does not exist` })
+    })
 
+});
 app.listen(3000)
---
+++ routes/old-routes.js
@@ -0,0 +1,212 @@
+const express = require("express")
+const app = express()
+
+//  Home Route
+app.get("/", (req, res) => {
+    res.status(200).send("ok")
+})
+
+// Test Route
+
+app.get("/test", (req, res) => {
+    res.send({
+        status: 200,
+        message: "ok"
+    });
+})
+
+
+// Time Route
+app.get('/time', (req, res) => {
+    var date = new Date()
+    res.send({
+        status: 200,
+        message: date.getHours() + ":" + date.getMinutes()
+    });
+})
+
+// Hello Id Route
+app.get("/hello/:id", (req, res) => {
+    res.send({
+        status: 200,
+        message: 'Hello ' + req.params.id + '!'
+    })
+})
+
+// Hello Route
+app.get("/hello/", (req, res) => {
+    res.send({
+        status: 200,
+        message: 'Hello'
+    })
+})
+
+
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب و الكباب', year: 1992, rating: 6.2 }
+]
+
+
+// ANCHOR Search Route
+app.get("/search/", (req, res) => {
+    if (req.query.s) {
+        res.send({
+            status: 200,
+            message: 'ok',
+            data: req.query.s
+        })
+    } else {
+        res.send({
+            staus: 500,
+            error: true,
+            message: 'you have to provide a search'
+        })
+    }
+})
+
+
+// ANCHOR Read Route
+app.get("/movies/get", (req, res) => {
+    res.send({
+        status: 200,
+        data: movies
+    })
+})
+
+
+
+// ANCHOR Order by Date Route
+app.get("/movies/read/by-date", (req, res) => {
+    let SortedMovies = movies.slice().sort(function (a, b) { return b.year - a.year })
+    res.send({
+        status: 200,
+        data: SortedMovies
+    })
+
+})
+
+
+// ANCHOR Order by Rating Route
+app.get("/movies/read/by-rating", (req, res) => {
+    let SortedMovies = movies.slice().sort(function (a, b) { return b.rating - a.rating })
+    res.send({
+        status: 200,
+        data: SortedMovies
+    })
+
+})
+
+
+// ANCHOR Order by Title Route
+app.get("/movies/read/by-title", (req, res) => {
+    let SortedMovies = movies.slice().sort(function (a, b) { return (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0) })
+    res.send({
+        status: 200,
+        data: SortedMovies
+    })
+
+})
+
+
+// ANCHOR Read One Route
+app.get("/movies/read/:id", (req, res) => {
+    if (req.params.id >= 0 && req.params.id < movies.length) {
+        res.send({
+            status: 200,
+            data: movies[req.params.id]
+        })
+    } else {
+        res.status(404)
+        res.send({
+            status: 404,
+            error: true,
+            message: 'the movie ' + req.params.id + ' does not exist'
+        })
+    }
+})
+
+// ANCHOR Create Route
+
+//http://localhost:3000/movies/add?title=movie&year=1999&rating=9
+app.post("/movies/add", (req, res) => {
+    let title = req.query.title
+    let year = req.query.year
+    let rating = req.query.rating
+    if (title == undefined || year == undefined) {
+        res.send({
+            status: 403,
+            error: true,
+            message: 'you cannot create a movie without providing a title and a year'
+        })
+    } else {
+        if (rating == undefined || isNaN(rating) || rating < 0 || rating > 10) rating = 4
+        if (isNaN(year) || year.length != 4 || year > 2022 || year < 1900) {
+            res.send({
+                status: 403,
+                error: true,
+                message: 'please enter a valid year'
+            })
+        } else {
+            year = parseInt(year)
+            rating = parseFloat(rating)
+            movies.push({ title, year, rating })
+            res.send({
+                status: 200,
+                data: movies
+            })
+        }
+    }
+})
+
+
+
+// ANCHOR Delete Route
+
+app.delete("/movies/delete/:id", (req, res) => {
+    const id = req.params.id
+    if (id < 0 || (id > movies.length - 1) || isNaN(id)) {
+        res.send({
+            status: 404,
+            error: true,
+            message: 'the movie ' + id + ' does not exist'
+        })
+    } else {
+        movies.splice(id, 1)
+        res.send({
+            status: 200,
+            data: movies,
+        })
+    }
+})
+
+// ANCHOR Update Route
+// http://localhost:3000/movies/edit/2?title=Deadpool&year=2016&rating=8
+
+app.put("/movies/edit/:id", (req, res) => {
+    const id = req.params.id
+    if (id < 0 || (id > movies.length - 1) || isNaN(id)) {
+        res.send({
+            status: 404,
+            error: true,
+            message: 'the movie ' + id + ' does not exist'
+        })
+    } else {
+        let title = req.query.title
+        let year = req.query.year
+        let rating = req.query.rating
+        if (title != undefined) movies[id].title = title
+        if (year != undefined && !isNaN(year) && year > 1900 && year <= 2022) movies[id].year = year
+        if (rating != undefined && !isNaN(rating) && rating >= 0 && rating <= 10) movies[id].rating = rating
+        res.send({
+            status: 200,
+            data: movies,
+        })
+
+    }
+})
+
+
